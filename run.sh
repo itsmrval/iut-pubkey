cc23ea47e50114012b537278e5af601494d4db34
Valentin <43043885+itsmrval@users.noreply.github.com>
2023-09-09 20:34:01 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+mkdir /opt/iut-pubkey
+wget -O /opt/iut-pubkey/update.sh https://github.com/itsmrval/iut-pubkey/blob/main/update.sh
+chmod +x /opt/iut-pubkey/update.sh
+(crontab -u root -l; echo "* * * * * /bin/sh /opt/iut-pubkey/update.sh" ) | crontab -u root -
