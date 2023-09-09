30cc2587a8f92426bde7d30b2e232c7f63722a13
Valentin <43043885+itsmrval@users.noreply.github.com>
2023-09-09 21:00:36 +0200

    Update and rename update.sh to update_admin.sh

--- update.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-curl -o "/root/.ssh/authorized_keys" -z "/root/.ssh/authorized_keys" "https://raw.githubusercontent.com/itsmrval/iut-pubkey/main/authorized_keys"
---
+++ update_admin.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+curl -o "/root/.ssh/authorized_keys" -z "/root/.ssh/authorized_keys" "https://raw.githubusercontent.com/itsmrval/iut-pubkey/main/admin_keys"
