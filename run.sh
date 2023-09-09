cd3765003467414b38dc49fdbeedfd85f599df1c
Valentin <43043885+itsmrval@users.noreply.github.com>
2023-09-09 21:07:32 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir /opt/iut-pubkey
-wget -O /opt/iut-pubkey/update.sh https://raw.githubusercontent.com/itsmrval/iut-pubkey/main/update.sh
+
+PS3='Choisissez le niveau d accès: '
+options=("admin" "team" "cancel")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "admin")
+            echo "utilisation de l'url admin"
+            wget -O /opt/iut-pubkey/update.sh https://raw.githubusercontent.com/itsmrval/iut-pubkey/main/update_admin.sh
+            break
+            ;;
+        "team")
+            echo "utilisation de l'url team"
+            wget -O /opt/iut-pubkey/update.sh https://raw.githubusercontent.com/itsmrval/iut-pubkey/main/update_team.sh
+            break
+
+            ;;
+        "cancel")
+            exit
+            ;;
+        *) echo "invalide";;
+    esac
+done
+
 chmod +x /opt/iut-pubkey/update.sh
 (crontab -u root -l; echo "* * * * * /bin/sh /opt/iut-pubkey/update.sh" ) | crontab -u root -
+
+
